0b1040c661425f0cb810c157d6a6e94c51940e0d
caduado13 <devcarlos.siqueira@gmail.com>
2025-06-01 18:22:37 -0300

    integração flask e login

--- app.py
+++ app.py
@@ -1,18 +1,78 @@
+#from app import ecom_register
+#from app import ecom_login
+#
+#menu_message = f'ECommerce, Bem vindo!\nDigite:\n[1] - Para logar na sua conta\n[2] - Registro de novo usuário\n[3] - Fechar o sistema!\nSua ação: '
+#count = 0
+#
+#while True:
+#    try:
+#        menu_gui = int(input(menu_message))
+#        if menu_gui == 1:
+#            ecom_login.login_menu(count)
+#            print(count)
+#        elif menu_gui == 2:
+#            ecom_register.register_user()
+#        elif menu_gui == 3:
+#            break
+#    except:
+#        input('[ERRO]Por favor digite um número!\nPressione enter para voltar ao sistema!')
+
+
+from flask import Flask
+from flask import render_template, request, url_for, redirect
+
+from app import regex
 from app import ecom_register
-from app import ecom_login
+from app import utils
 
-menu_message = f'ECommerce, Bem vindo!\nDigite:\n[1] - Para logar na sua conta\n[2] - Registro de novo usuário\n[3] - Fechar o sistema!\nSua ação: '
-count = 0
+app = Flask(__name__)
 
-while True:
-    try:
-        menu_gui = int(input(menu_message))
-        if menu_gui == 1:
-            ecom_login.login_menu(count)
-            print(count)
-        elif menu_gui == 2:
-            ecom_register.register_user()
-        elif menu_gui == 3:
-            break
-    except:
-        input('[ERRO]Por favor digite um número!\nPressione enter para voltar ao sistema!')
+@app.route("/register")
+def register_render():
+    return render_template('register.html')
+
+@app.route("/register-form", methods = ['POST'])
+def register_form():
+    if request.method == 'POST':
+        name = request.form['reg-name']
+        email = request.form['reg-email']
+        password = request.form['reg-password']
+        cpf = request.form['reg-cpf']
+        
+        is_valid_email = regex.check_email(email)
+        is_valid_password = regex.check_pass(password)
+        is_valid_cpf = regex.check_cpf(cpf)
+
+        if is_valid_email and is_valid_password and is_valid_cpf:
+            ecom_register.register_user(name=name, password=password, email=email, cpf=cpf, db_path='./db/project.db')
+            return redirect(url_for('login_render'))
+    else:
+        print('erro aqui')
+        return "Este endpoint aceita apenas requisições POST."
+
+@app.route("/login")
+def login_render():
+    return render_template('login.html')
+
+@app.route("/login-form", methods = ['POST'])
+def login_form():
+    if request.method == 'POST':
+        email = request.form['log-email']
+        password = request.form['log-password']
+        db_path = './db/project.db'
+        login_result = utils.check_login(email, password, db_path)
+
+        if login_result is True:
+            return redirect(url_for("home_render"))
+        else:
+            return f'Login errado'
+    else:
+        return f'Erro de login'
+
+
+@app.route("/home")
+def home_render():
+    return render_template("home.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- app/utils.py
+++ app/utils.py
@@ -4,33 +4,26 @@ import traceback
 import sys
 
 
-def check_login(email, password, flag= False):
+def check_login(email, password, db_path):
     try:
-        with sqlite3.connect("../db/project.db") as conn:
-            cursor = conn.cursor()
+        with sqlite3.connect(db_path) as connect:
+            cursor = connect.cursor()
             cursor.execute("SELECT email, senha FROM users WHERE email = ?", (email,))
             user = cursor.fetchone()
-            while True:
-                if user:
-                    email_stored, password_stored = user
-                    if bcrypt.checkpw(password.encode('utf-8'), password_stored):
-                        print("Conectado!")  
-                        flag = True
-                        break
-                    else:
-                        print("Senha incorreta!")
-                        password = input('Tente novamente: ')
+            if user:
+                email_stored, password_stored = user
+                if bcrypt.checkpw(password.encode('utf-8'), password_stored):
+                    return True #Senha correta!
                 else:
-                    print("Usuário não encontrado!")
-                    break
+                    return False #Senha incorreta!
+            else:
+                return False #Usuário não encontrado
+                
     except Exception as e:
         # Exibe o erro completo no terminal
         print("\n--- DEBUG (ERRO INTERNO) ---", file=sys.stderr)
         traceback.print_exc()  # Mostra o stack trace completo
         print("----------------------------", file=sys.stderr)
         #Erro amigável ao usuário
-        input('[ERRO]Por favor confira seus dados e tente novamente. \nPressione enter para voltar ao sistema!')
-
-    return flag
-
+        
 
